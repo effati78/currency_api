f682c14a908ad71e30d62333f29c5477b194c525
REZA <56348113+effati78@users.noreply.github.com>
2020-12-11 19:03:15 +0330

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const url = 'https://www.megaweb.ir/api/money';
-var ajax;
 var table = document.querySelector('.tbody');
 var count = 1;
 
 if (window.XMLHttpRequest) {
-    ajax = new XMLHttpRequest()
+    var ajax = new XMLHttpRequest()
 } else {
-    ajax = new ActiveXObject('Microsoft.XMLHTTP')
+    var ajax = new ActiveXObject('Microsoft.XMLHTTP')
 }
 
 ajax.open('GET', url, true);
@@ -14,10 +13,8 @@ ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 ajax.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var res = JSON.parse(this.responseText);
-        console.log(res);
-        debugger;
         for (i in res) {
-            table.innerHTML += `<tr> <td>${count}</td> <td>${res[i].title}</td> <td>${res[i].price}</td> <td>${[res[i].jdate]}</td></tr>`;
+            table.innerHTML += `<tr class='item${count}'> <td>${count}</td> <td>${res[i].title}</td> <td>${res[i].price}</td> <td>${[res[i].jdate]}</td></tr>`;
             count++;
         }
     } else if (this.readyState != 4 && this.status != 200) {
